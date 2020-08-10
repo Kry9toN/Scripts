1eecec0a6e2b64a49b766f4682a0a76b6587d0d6
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-08-10 20:57:02 +0700

    update script jenkins

--- linux/server.sh
+++ linux/server.sh
@@ -7,11 +7,11 @@ while true; do
             # Format and setup
             echo "***** Prepare Persistant Disk path:/mnt/build *****  ";
             sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb;
-            sudo mkdir -p /mnt/build;
-            sudo mount -o discard,defaults /dev/sdb /mnt/build;
+            sudo mkdir -p /mnt/;
+            sudo mount -o discard,defaults /dev/sdb /mnt/;
             sudo chmod a+w /mnt/build;
             sudo cp /etc/fstab /etc/fstab.backup;
-            echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/build ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab;
+            echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/ ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab;
             cat /etc/fstab; break;;
         [Nn]* ) break;;
         * ) echo "Please answer y or n.";;
@@ -36,7 +36,7 @@ echo "***** Enviroment setup.. *****"
 sudo git config --global user.name "Kry9toN"
 sudo git config --global user.email "dhimasbagusprayoga@gmail.com"
 sudo /usr/sbin/update-ccache-symlinks
-echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc
+echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.zshrc
 source ~/.bashrc && echo $PATH
 sleep 1s
 
@@ -86,7 +86,13 @@ sudo apt-get update
 sudo apt install certbot python3-certbot-apache -y
 sudo adduser gerrit --gecos "" --disabled-password
 echo "gerrit:gerrit" | sudo chpasswd
-sudo -u gerrit -H sh -c "cd $HOME; wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1eV2q-YpOSwMaFyoyeKqxjq-HJpgWRy7l' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1eV2q-YpOSwMaFyoyeKqxjq-HJpgWRy7l" -O gerrit.zip && rm -rf /tmp/cookies.txt; unzip gerrit.zip; sed -i 's/8080/2222/g' $HOME/gerrit/etc/gerrit.config; $HOME/gerrit/bin/gerrit.sh start; "
+sudo -i -u someuser bash << EOF
+cd$HOME
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1eV2q-YpOSwMaFyoyeKqxjq-HJpgWRy7l' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1eV2q-YpOSwMaFyoyeKqxjq-HJpgWRy7l" -O gerrit.zip && rm -rf /tmp/cookies.txt
+unzip gerrit.zip
+sed -i 's/8080/2222/g' /home/gerrit/gerrit/etc/gerrit.config
+/home/gerrit/gerrit/bin/gerrit.sh start
+EOF
 
 echo "***** Setup apache2 for gerrit *****"
 sudo wget https://raw.githubusercontent.com/Komodo-OS-Rom/Server_Utilites/master/apache2/gerrit.conf -P /etc/apache2/sites-available/
