111e9bb6354a74fe8d159852e0c560c4c02626e2
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-01-06 15:41:16 +0700

    Make variabel and remove useless code

--- komodo/buildbot.sh
+++ komodo/buildbot.sh
@@ -48,9 +48,12 @@ path_ccache="$(pwd)/.ccache"
 
 CDIR=$PWD
 OUT="${CDIR}/out/target/product/miatoll"
-ROM_NAME="KomodoOS"
+ROM_NAME="komodo"
 DEVICE="miatoll"
+BUILD_TYPE="eng"
 KOMODOFILE="/home/jenkins/file"
+
+server=$(uname -u)
 
 # my Time
 export TZ=":Asia/Jakarta"
@@ -201,29 +204,21 @@ fi
 timeStart
 build_message() {
 	if [ "$CI_MESSAGE_ID" = "" ]; then
-CI_MESSAGE_ID=$(tg_send_message --chat_id "$CHAT_ID" --text "<b>====== Starting Build Komodo 🦎 ======</b>
-<b>ROM Name:</b> <code>${ROM_NAME}</code>
-<b>Branch:</b> <code>${BRANCH_MANIFEST}</code>
-<b>Device:</b> <code>${DEVICE}</code>
-<b>Type:</b> <code>$build_type</code>
-<b>Command:</b> <code>$target_command</code>
-<b>Cleaning:</b> <code>$make_clean</code>
-<b>Job:</b> <code>$jobs Paralel processing</code>
-<b>Upload to SF:</b> <code>$upload_to_sf</code>
+CI_MESSAGE_ID=$(tg_send_message --chat_id "$CHAT_ID" --text "<b>======== Starting Build Rom ========</b>
+<b>ROM Name:</b> <code>$ROM_NAME</code>
+<b>Branch:</b> <code>$BRANCH_MANIFEST</code>
+<b>Device:</b> <code>$DEVICE</code>
+<b>Type:</b> <code>$BUILD_TYPE</code>
 <b>Running on:</b> <code>$server</code>
 <b>Started at</b> <code>$DATE</code>
 
 <b>Status:</b> $1" --parse_mode "html" | jq .result.message_id)
 	else
-tg_edit_message_text --chat_id "$CHAT_ID" --message_id "$CI_MESSAGE_ID" --text "<b>====== Starting Build Komodo 🦎 ======</b>
-<b>ROM Name:</b> <code>${ROM_NAME}</code>
-<b>Branch:</b> <code>${BRANCH_MANIFEST}</code>
-<b>Device:</b> <code>${DEVICE}</code>
-<b>Type:</b> <code>$build_type</code>
-<b>Command:</b> <code>$target_command</code>
-<b>Cleaning:</b> <code>$make_clean</code>
-<b>Job:</b> <code>$jobs Paralel processing</code>
-<b>Upload to SF:</b> <code>$upload_to_sf</code>
+tg_edit_message_text --chat_id "$CHAT_ID" --message_id "$CI_MESSAGE_ID" --text "<b>======== Starting Build Rom ========</b>
+<b>ROM Name:</b> <code>$ROM_NAME</code>
+<b>Branch:</b> <code>$BRANCH_MANIFEST</code>
+<b>Device:</b> <code>$DEVICE</code>
+<b>Type:</b> <code>$BUILD_TYPE</code>
 <b>Running on:</b> <code>$server</code>
 <b>Started at</b> <code>$DATE</code>
 
@@ -317,15 +312,15 @@ BUILDLOG="$CDIR/out/${ROM_NAME}-miatoll-${DATELOG}.log"
 # time to build bro
 build_message "Staring broo...🔥"
 source build/envsetup.sh
-build_message "lunch komodo_miatoll-eng"
-lunch komodo_miatoll-eng
+build_message "lunch "$ROM_NAME"_"$DEVICE"-"BUILD_TYPE""
+lunch "$ROM_NAME"_"$DEVICE"-"BUILD_TYPE""
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "masak masak"
 sleep 2
 build_message "🛠️ Building..."
 progress &
-masak "$target_command" -j"$jobs" > reading
+masak komodo > reading
 
 # Record exit code after build
 retVal=$?
